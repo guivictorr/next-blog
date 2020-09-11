ec0c7b61cf72bec708fe04bd8f51903c36978580
Guilherme <guilhermevictor20@gmail.com>
2020-09-10 22:20:00 -0300

    created posts folder and fetched to home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
+import { getSortedPostsData } from '../lib/posts'
+
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +25,21 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
