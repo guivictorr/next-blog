ab7a3046a96c6f4f336e7d327634325d68720bc4
guivictorr <guilhermevictor20@gmail.com>
2021-03-24 11:56:50 -0300

    feat(posts): posts types and fetchind posts with getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
+import { GetStaticProps } from 'next';
+import { PostData } from '../@types/posts';
+
+type HomeProps = {
+  posts: PostData[];
+};
+
+const getPosts = async (): Promise<PostData> => {
+  const posts = await fetch('http://limitless-cove-88022.herokuapp.com/posts');
+  const jsonPosts = posts.json();
+
+  return jsonPosts;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+const Home = ({ posts }: HomeProps) => {
   return <h1>Hello World</h1>;
-}
+};
+
+export default Home;
